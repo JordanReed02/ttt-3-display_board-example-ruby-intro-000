9dc87875f6ac157ea00f08d1631b2354e2eac367
Jordan Reed <jordanalireed@icloud.com>
2020-06-17 06:07:52 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,12 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+ end
+ board = (n,val)
+board 'n.times'
+row_index
+  row= []
+  n.times { |column_index| row << val } # or row.push(val)
+  board << row # or board.push(val)
+ board
+(display_board '3, nil'
+)#=> [[nil, nil, nil], [nil, nil, nil,], [nil, nil, nil]]
